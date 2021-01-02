eb62d39fc5a0a0449b167fcf31e73b529592184a
ahmed_safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2021-01-02 22:50:11 +0200

    Patch 3

--- HCIA Project/action.js
+++ HCIA Project/action.js
@@ -1,1 +1,15 @@
 
+  const api_url ='https://api.covidtracking.com/v1/us/current.json';     
+  fetchData();
+  async function fetchData(){
+      const resposne =  await fetch(api_url);
+      const data = await resposne.json();
+      console.log(data);
+      document.getElementById('death').textContent=numberWithCommas(data[0].death);
+      document.getElementById('recovered').textContent=numberWithCommas(data[0].recovered);
+      document.getElementById('postive').textContent=numberWithCommas(data[0].positive);
+      
+  }
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
