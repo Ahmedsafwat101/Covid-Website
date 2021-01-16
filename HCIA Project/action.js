387612ab8576b28e2723bb0fe40f1c4a3f68d725
ahmed_safwat <42112466+Ahmedsafwat101@users.noreply.github.com>
2021-01-16 20:00:20 +0200

    Final Patch

--- HCIA Project/action.js
+++ HCIA Project/action.js
@@ -6,10 +6,11 @@
       const data = await resposne.json();
       console.log(data);
       document.getElementById('death').textContent=numberWithCommas(data[0].death);
-      document.getElementById('recovered').textContent=numberWithCommas(data[0].recovered);
-      document.getElementById('postive').textContent=numberWithCommas(data[0].positive);
+      document.getElementById('recovered').textContent=numberWithCommas(data[0].hospitalized);
+      document.getElementById('positive').textContent=numberWithCommas(data[0].positive);
       
   }
   function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    console.log(x);
+     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   }
